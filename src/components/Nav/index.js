b8fcb8d7c676bbc9340739bb0ef05a7c4d26a0f9
tyarchy <ty.d.gregory@gmail.com>
2022-05-08 20:39:37 -0600

    categories

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,38 +2,63 @@ import React from 'react';
 
 function Nav() {
 
+  function categorySelected(name) {
+    console.log(`${name} clicked`)
+  }
+
+  const categories = [
+    {
+      name: "SCENERY",
+      description: "farts"
+    },
+
+    {
+      name: "PORTFOLIO",
+      description: "Portraits of people in my life"
+    },
+
+    {
+      name: "LANDSCAPE",
+      description: "Fields,",
+    },
+  ];
+
   return (
     <header>
-  <h2>
-    <a href="/">
-     Tyler Gregory
-    </a>
-  </h2>
-  <nav>
-    <ul className="flex-row">
-      <li className="mx-2">
-        <a href="#about">
-          ABOUT ME
+      <h2>
+        <a href="/">
+          Tyler Gregory
         </a>
-      </li>
-      <li className="mx-2">
-        <a href="#about">
-          PORTFOLIO
-        </a>
-      </li>
-      <li className="mx-2">
-        <a href="#about">
-         RESUME
-        </a>
-      </li>
-      <li>
-      <a href="#about">
-          CONTACT
-        </a>
-      </li>
-    </ul>
-  </nav>
-</header>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+        {categories.map((category) => (
+            <li
+              className="mx-1"
+              key={category.name}
+            >
+              <span onClick={categorySelected} >
+                {category.name}
+              </span>
+            </li>
+          ))}
+          <li className="mx-2">
+            <a href="#about">
+              ABOUT ME
+            </a>
+          </li>
+          <li className="mx-2">
+            <a href="#about">
+              RESUME
+            </a>
+          </li>
+          <li>
+            <span>CONTACT</span>
+          </li>
+       
+        </ul>
+      </nav>
+    </header>
   );
 }
 
