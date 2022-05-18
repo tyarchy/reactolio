4044d312317dc890245dc3bba93b6e22b73d8528
tyarchy <ty.d.gregory@gmail.com>
2022-05-17 20:38:10 -0600

    done for now

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Header() {
+  return (
+    <header class="portfolio">WELCOME TO THE REACTOLIO<br></br>
+    <h1>TYLER GREGORY WEB DEVELOPMENT</h1></header>
+  );
+}
+
+export default Header;
